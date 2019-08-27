98ed71b38695d5a8d077535e6cbcb0a9e4f54a20
mfv-computer-0027 <mfv-computer-0027@mfv-computer-0027.local>
2019-08-27 14:10:52 +0700

    Upgrade to tf v0.12

--- service_load_balancing/variables.tf
+++ service_load_balancing/variables.tf
@@ -28,7 +28,7 @@ variable "deployment_minimum_healthy_percent" {
 
 variable "network_mode" {
   description = "The Docker networking mode to use for the containers in the task"
-  type        = "string"
+  type        = string
   default     = "bridge" // none / bridge / awsvpc / host
 }
 
@@ -69,38 +69,38 @@ variable "iam_role_inline_policy" {
   ]
 }
 EOT
-}
 
+}
 
 # ECS launch_type: FARGATE
 
 variable "subnet_ids" {
   description = "AWS vpc zone identifier(s)"
-  type        = "list"
-  default     = []
+  type = list(string)
+  default = []
 }
 
 variable "security_group_ids" {
   description = "AWS security group id(s) for container instances launch configuration"
-  type        = "list"
-  default     = []
+  type = list(string)
+  default = []
 }
 
 # ECS launch_type: EC2
 
 variable "cluster_id" {
   description = "ID (ARN) of ECS Cluster if created by external"
-  default     = ""
+  default = ""
 }
 
 variable "placement_strategy_type" {
   description = "The type of placement strategy"
-  default     = "spread" // or binpack (this module cannot specify "random"
+  default = "spread" // or binpack (this module cannot specify "random"
 }
 
 variable "placement_strategy_field" {
   description = "The field of placement strategy valid values that U select type"
-  default     = "instanceId"
+  default = "instanceId"
 }
 
 # Application Load Balancer
@@ -121,29 +121,29 @@ variable "container_port" {
 
 variable "container_family" {
   description = "AWS containers family name"
-  type        = "string"
+  type = string
 }
 
 variable "container_definitions" {
   description = "AWS ECS Task definitions"
-  type        = "string"
+  type = string
 }
 
 variable "log_groups" {
   description = "The List of CloudWatch Log Group Name for ECS Task (Container)"
-  type        = "list"
-  default     = []
+  type = list(string)
+  default = []
 }
 
 variable "log_groups_expiration_days" {
   description = "Specifies the number of days you want to retain log events"
-  default     = 30
+  default = 30
 }
 
 variable "log_groups_tags" {
   description = "The tags of cloudwatch log group"
-  type        = "map"
-  default     = {}
+  type = map(string)
+  default = {}
 }
 
 # AutoScaling
@@ -153,98 +153,99 @@ variable "autoscale_iam_role_arn" {
   # $ aws iam create-service-linked-role --aws-service-name ecs.amazonaws.com
   # Ref: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/using-service-linked-roles.html
   description = "Specifies the IAM Role for ecs appautoscaling"
-  default     = "" // not creates if empty
+  default = "" // not creates if empty
 }
 
 variable "autoscale_max_capacity" {
   description = "The max capacity of the scalable target"
-  default     = 1
+  default = 1
 }
 
 variable "autoscale_min_capacity" {
   description = "The min capacity of the scalable target"
-  default     = 1
+  default = 1
 }
 
 variable "autoscale_cooldown" {
   description = "The cooldown / period for watching scale (seconds)"
-  default     = 300
+  default = 300
 }
 
 # AutoScaling > scale-out
 
 variable "scale_out_thresholds" {
   description = "The values against which the specified statistic is compared for scale_out"
-  type        = "map"
+  type = map(string)
+
   # No apply if empty
-  default     = {
-    # Supporting thresholds as berow
-    #cpu    = // e.g. 75
-    #memory = // e.g. 75
-  }
+  default = {}
+  # Supporting thresholds as berow
+  #cpu    = // e.g. 75
+  #memory = // e.g. 75
 }
 
 variable "scale_out_step_adjustment" {
   description = "The attributes of step scaling policy"
-  type        = "map"
-  default     = {
+  type = map(string)
+  default = {
     metric_interval_lower_bound = 0
-    scaling_adjustment          = 1
+    scaling_adjustment = 1
   }
 }
 
 variable "scale_out_evaluation_periods" {
   description = "The number of evaluation periods to scale out"
-  default     = 1
+  default = 1
 }
 
 variable "scale_out_ok_actions" {
   description = "For scale-out as same as ok actions for cloudwatch alarms"
-  type        = "list"
-  default     = []
+  type = list(string)
+  default = []
 }
 
 variable "scale_out_more_alarm_actions" {
   description = "For scale-out as same as alarm actions for cloudwatch alarms"
-  type        = "list"
-  default     = []
+  type = list(string)
+  default = []
 }
 
 # AutoScaling > scale-in
 
 variable "scale_in_ok_actions" {
   description = "For scale-in as same as ok actions for cloudwatch alarms"
-  type        = "list"
-  default     = []
+  type = list(string)
+  default = []
 }
 
 variable "scale_in_more_alarm_actions" {
   description = "For scale-in as same as alarm actions for cloudwatch alarms"
-  type        = "list"
-  default     = []
+  type = list(string)
+  default = []
 }
 
 variable "scale_in_evaluation_periods" {
   description = "The number of evaluation periods to scale in"
-  default     = 2
+  default = 2
 }
 
 variable "scale_in_thresholds" {
   description = "The values against which the specified statistic is compared for scale_in"
-  type        = "map"
+  type = map(string)
+
   # No apply if empty
-  default     = {
-    # Supporting thresholds as berow
-    #cpu    = // e.g.  5
-    #memory = // e.g. 40
-  }
+  default = {}
+  # Supporting thresholds as berow
+  #cpu    = // e.g.  5
+  #memory = // e.g. 40
 }
 
 variable "scale_in_step_adjustment" {
   description = "The attributes of step scaling policy"
-  type        = "map"
-  default     = {
+  type = map(string)
+  default = {
     metric_interval_upper_bound = 0
-    scaling_adjustment          = -1
+    scaling_adjustment = -1
   }
 }
+
