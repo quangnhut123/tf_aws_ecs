d1b1170388c0cb8e7142fce24a2ba59108064566
quangnhnhut123 <quangnhnhut@gmail.com>
2020-09-16 16:27:05 +0700

    Adding task role output

--- service_load_balancing/outputs.tf
+++ service_load_balancing/outputs.tf
@@ -10,3 +10,11 @@ output "log_group_arn" {
   value = aws_cloudwatch_log_group.app[0].arn
 }
 
+output "fargate_ecs_task_role" {
+  value = length(aws_iam_role.fargate_ecs_task_role) > 0 ? aws_iam_role.fargate_ecs_task_role[0].arn : ""
+}
+
+output "fargate_ecs_task_execution_role" {
+  value = length(aws_iam_role.fargate_ecs_task_execution_role) > 0 ? aws_iam_role.fargate_ecs_task_execution_role[0].arn : ""
+}
+
