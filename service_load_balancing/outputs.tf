2e4485cd7a0ab277353e145b1e8ca76592978ddf
Tran Quang Nhut (Jonny) <tran.quang.nhut@moneyforward.vn>
2022-06-02 14:37:22 +0700

    Update outputs.tf
    Update outputs for no_alb case

--- service_load_balancing/outputs.tf
+++ service_load_balancing/outputs.tf
@@ -25,3 +25,19 @@ output "fargate_ecs_task_role_name" {
 output "fargate_ecs_task_execution_role_name" {
   value = length(aws_iam_role.fargate_ecs_task_execution_role) > 0 ? aws_iam_role.fargate_ecs_task_execution_role[0].name : ""
 }
+
+output "fargate_no_alb_ecs_task_role_arn" {
+  value = length(aws_iam_role.fargate_no_alb_ecs_task_role) > 0 ? aws_iam_role.fargate_no_alb_ecs_task_role[0].arn : ""
+}
+
+output "fargate_no_alb_ecs_task_execution_role_arn" {
+  value = length(aws_iam_role.fargate_no_alb_ecs_task_execution_role) > 0 ? aws_iam_role.fargate_no_alb_ecs_task_execution_role[0].arn : ""
+}
+
+output "fargate_no_alb_ecs_task_role_name" {
+  value = length(aws_iam_role.fargate_no_alb_ecs_task_role) > 0 ? aws_iam_role.fargate_no_alb_ecs_task_role[0].name : ""
+}
+
+output "fargate_no_alb_ecs_task_execution_role_name" {
+  value = length(aws_iam_role.fargate_no_alb_ecs_task_execution_role) > 0 ? aws_iam_role.fargate_no_alb_ecs_task_execution_role[0].name : ""
+}
