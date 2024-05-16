a4efe16e70ae1c4d903d16c97e06652157b1b130
Jay (Nguyen Nhu Lan) <56527890+jay-87@users.noreply.github.com>
2024-05-16 14:22:52 +0700

    Revert "add additional s3 policy to task role"

--- cluster/iam.tf
+++ cluster/iam.tf
@@ -21,43 +21,6 @@ EOF
 
 }
 
-resource "aws_iam_policy" "s3_policy" {
-  count  = var.s3_policy ? 1 : 0
-  name   = "additional-s3-policy"
-  policy = data.aws_iam_policy_document.s3_policy_document[count.index].json
-}
-
-data "aws_iam_policy_document" "s3_policy_document" {
-  count  = var.s3_policy ? 1 : 0
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:ListBucket",
-      "s3:GetBucketLocation"
-    ]
-    resources = var.s3_policy && length(var.bucket_arn_list) > 0 ? var.bucket_arn_list : null
-
-  }
-
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:*",
-    ]
-    resources = var.s3_policy && length(var.bucket_arn_list) > 0 ? [ for arn in var.bucket_arn_list : "${arn}/*" ] : null
-
-  }
-}
-
-resource "aws_iam_policy_attachment" "s3_policy_attach" {
-  count = var.s3_policy ? 1 : 0
-  name  = "additional-s3-policy-attach"
-
-  roles = aws_iam_role.ecs_instance.name
-
-  policy_arn = aws_iam_policy.s3_policy[count.index].arn
-}
-
 resource "aws_iam_role_policy_attachment" "ecs_instance_policy" {
   count = length(var.iam_policy_arns)
 
