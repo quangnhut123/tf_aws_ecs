ae1a35712abc713262865da8643b61cc79cca294
Jay (Nguyen Nhu Lan) <56527890+jay-87@users.noreply.github.com>
2024-05-16 14:21:43 +0700

    Revert "fix array condition"

--- service_load_balancing/launch_type_fargate.tf
+++ service_load_balancing/launch_type_fargate.tf
@@ -133,14 +133,14 @@ resource "aws_iam_role_policy" "fargate_s3" {
         "s3:ListBucket",
         "s3:GetBucketLocation"
       ],
-      "Resource": ${var.s3_policy} && length(${var.bucket_arn_list}) > 0 ? ${var.bucket_arn_list} : ["null"]
+      "Resource": ${var.s3_policy} && length(${var.bucket_arn_list}) > 0 ? ${var.bucket_arn_list} : ""
     },
     {
       "Effect": "Allow",
       "Action": [
         "s3:*"
       ],
-      "Resource": ${var.s3_policy} && length(${var.bucket_arn_list}) > 0 ? [ for arn in ${var.bucket_arn_list} : "${arn}/*" ] : ["null"]
+      "Resource": ${var.s3_policy} && length(${var.bucket_arn_list}) > 0 ? [ for arn in ${var.bucket_arn_list} : "${arn}/*" ] : ""
     }
   ]
 }
